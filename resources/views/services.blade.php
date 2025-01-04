2b2433b09a3a12dd9c0162b0bbeb7661b5afe2c6
Aymane hr <aymanhar20@gmail.com>
2025-01-04 19:38:40 +0100

    create Model & migrations laravel

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,7 +1,11 @@
 <x-master1 title="Services">
 
-<x-alert type="warning">
-<strong>ach kadir </strong> fin awa ghadi !
+<x-alert type="danger">
+    <strong>ach kadir </strong> fin awa ghadi !
+</x-alert>
+
+<x-alert type="success">
+    <strong>Success!</strong> Indicates a successful .
 </x-alert>
 
 </x-master1>
